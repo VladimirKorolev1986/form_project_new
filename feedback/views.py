d2574aeb25ee34d53928c7bd4d2038afc4769101
VladimirKorolev <i@devkorolev.ru>
2023-05-08 11:37:26 +0300

    used django forms

--- feedback/views.py
+++ feedback/views.py
@@ -1,17 +1,19 @@
 from django.shortcuts import render
 from django.http import HttpResponseRedirect
+from .forms import FeedbackForm
 
 
 # Create your views here.
 
 def index(request):
+    form = FeedbackForm()
     if request.method == 'POST':
-        name = request.POST['name']
-        if len(name) == 0:
-            return render(request, 'feedback/feedback.html', context={'got_error': True})
-        print(name)
-        return HttpResponseRedirect('/done')
-    return render(request, 'feedback/feedback.html', context={'got_error': False})
+        form = FeedbackForm(request.POST)
+        if form.is_valid():
+            print(form.cleaned_data)
+            return HttpResponseRedirect('/done')
+    form = FeedbackForm()
+    return render(request, 'feedback/feedback.html', context={'form': form})
 
 def done(request):
     return render(request, 'feedback/done.html')
