9b258635fa30c73c527bce954d7b07cde3835fb8
VladimirKorolev <i@devkorolev.ru>
2023-05-09 15:17:47 +0300

    ModelForm

--- feedback/forms.py
+++ feedback/forms.py
@@ -1,13 +1,67 @@
 from django import forms
+from .models import Feedback
 
 
-class FeedbackForm(forms.Form):
-    name = forms.CharField(label='Имя', max_length=7, min_length=2, error_messages={
-        'max_length': 'Слишком много символов',
-        'min_length': 'Слишком мало символов',
-        'required': 'Укажите  хотя бы один символ'
+# class FeedbackForm(forms.Form):
+#     name = forms.CharField(label='Имя', max_length=7, min_length=2, error_messages={
+#         'max_length': 'Слишком много символов',
+#         'min_length': 'Слишком мало символов',
+#         'required': 'Укажите  хотя бы один символ'
+#
+#     })
+#     surname = forms.CharField(label='Фамилия')
+#     feedback = forms.CharField(label='Отзыв', widget=forms.Textarea(attrs={'rows': 2, 'cols': 40}))
+#     rating = forms.IntegerField(label='Рейтинг', max_value=5, min_value=1)
 
-    })
-    surname = forms.CharField(label='Фамилия')
-    feedback = forms.CharField(label='Отзыв', widget=forms.Textarea(attrs={'rows': 2, 'cols': 40}))
-    rating = forms.IntegerField(label='Рейтинг', max_value=5, min_value=1)
+# class FeedbackForm(forms.ModelForm):
+#     class Meta:
+#         model = Feedback
+#         # fields = ['name', 'surname', 'feedback', 'rating']
+#         fields = "__all__"
+#         labels = {
+#             'name': 'Имя',
+#             'surname': 'Фамилия',
+#             'feedback': 'Отзыв',
+#             'rating': 'Рейтинг'
+#         }
+#         # error_messages = {
+#         #     'name': {'max_length': 'ой как много символов',
+#         #              'min_length': 'ой как мало символов',
+#         #              'required': 'Не должно быть пустым'
+#         #              },
+#         #     'surname': {'max_length': 'ой как много символов',
+#         #                 'min_length': 'ой как мало символов',
+#         #                 'required': 'Не должно быть пустым'
+#         #                 },
+#         #     'feedback': {'max_length': 'ой как много символов',
+#         #                  'min_length': 'ой как мало символов',
+#         #                  'required': 'Не должно быть пустым'
+#         #                  }
+#         # }
+
+class FeedbackForm(forms.ModelForm):
+    class Meta:
+        model = Feedback
+        # fields = ['name', 'rating', 'surname']
+        fields = '__all__'
+        labels = {
+            'name': 'Имя',
+            'surname': 'Фамилия',
+            'feedback': 'Отзыв',
+            'rating': 'Рейтинг'
+        }
+
+        error_messages = {
+            'name': {'max_length': 'ой как много символов',
+                     'min_length': 'ой как мало символов',
+                     'required': 'Не должно быть пустым'
+                     },
+            'surname': {'max_length': 'ой как много символов',
+                        'min_length': 'ой как мало символов',
+                        'required': 'Не должно быть пустым'
+                        },
+            'feedback': {'max_length': 'ой как много символов',
+                         'min_length': 'ой как мало символов',
+                         'required': 'Не должно быть пустым'
+                         }
+        }
--- feedback/views.py
+++ feedback/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponseRedirect
 from .forms import FeedbackForm
+from .models import Feedback
 
 
 # Create your views here.
@@ -10,10 +11,12 @@ def index(request):
         form = FeedbackForm(request.POST)
         if form.is_valid():
             print(form.cleaned_data)
+            form.save()
             return HttpResponseRedirect('/done')
     else:
         form = FeedbackForm()
     return render(request, 'feedback/feedback.html', context={'form': form})
+
 
 def done(request):
     return render(request, 'feedback/done.html')
