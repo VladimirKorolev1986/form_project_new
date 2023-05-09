a2e8d498d660b31e4760f5e9eb00813eccdfd091
VladimirKorolev <i@devkorolev.ru>
2023-05-09 17:51:21 +0300

    new commit

--- feedback/urls.py
+++ feedback/urls.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 from django.urls import path
-from .views import index, done
+from .views import index, done, update_feedback
 
 urlpatterns = [
     path('done', done),
     path('', index),
+    path('<int:id_feedback>', update_feedback),
 ]
--- feedback/views.py
+++ feedback/views.py
@@ -20,3 +20,16 @@ def index(request):
 
 def done(request):
     return render(request, 'feedback/done.html')
+
+
+def update_feedback(request, id_feedback):
+    feed = Feedback.objects.get(id=id_feedback)
+    if request.method == 'POST':
+        form = FeedbackForm(request.POST, instance=feed)
+        if form.is_valid():
+            print(form.cleaned_data)
+            form.save()
+            return HttpResponseRedirect(f'/{id_feedback}')
+    else:
+        form = FeedbackForm(instance=feed)
+    return render(request, 'feedback/feedback.html', context={'form': form})
